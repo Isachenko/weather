a326039e681b0588d7185e1849ed5e6b491496b9
isaac <isaac@isaac-U43SD.(none)>
2014-03-05 17:08:00 +0300

    all done

--- pogoda.sh
+++ pogoda.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 . $PWD/pogoda.conf
-echo $DELAY
+
 while (true)
 do
-	if [ -f $PAGEFILE ]; then rm $PAGEFILE; fi
+	#if [ -f $PAGEFILE ]; then rm $PAGEFILE; fi
 	wget -t 1 -O $PAGEFILE http://www.gismeteo.by/weather-minsk-4248/
-	
+	grep -m 1 "<dd class='value m_temp c'>" $PAGEFILE > temp
+	echo "temperature: " $(sed 's/[^\-\+0-9]//g' temp)
+	rm temp
+	sleep $DELAY
 done
