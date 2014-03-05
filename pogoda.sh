a863066bd7ffe89fb75a6e4a0399b5757b347868
isaac <isaac@isaac-U43SD.(none)>
2014-03-05 17:10:58 +0300

    logs added

--- pogoda.sh
+++ pogoda.sh
@@ -4,7 +4,7 @@
 while (true)
 do
 	#if [ -f $PAGEFILE ]; then rm $PAGEFILE; fi
-	wget -t 1 -O $PAGEFILE http://www.gismeteo.by/weather-minsk-4248/
+	wget -t 1 -O $PAGEFILE -o wget.log http://www.gismeteo.by/weather-minsk-4248/
 	grep -m 1 "<dd class='value m_temp c'>" $PAGEFILE > temp
 	echo "temperature: " $(sed 's/[^\-\+0-9]//g' temp)
 	rm temp
